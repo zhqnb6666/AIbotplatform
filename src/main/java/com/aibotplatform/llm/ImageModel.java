3a7b9375c63d3d1459ee9b6580463feb31dbfef3
VictorJiang <2380952232@qq.com>
2024-10-23 11:22:10 +0800

    add image model

---
+++ src/main/java/com/aibotplatform/llm/ImageModel.java
@@ -0,0 +1,41 @@
+package com.aibotplatform.llm;
+
+import com.baidubce.qianfan.Qianfan;
+import com.baidubce.qianfan.model.image.Text2ImageResponse;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.util.UUID;
+
+import static com.baidubce.qianfan.core.auth.Auth.TYPE_OAUTH;
+
+public class ImageModel {
+    private static final String ak = "uMF5PVIQDQYY58QZJ0J04XrF";
+    private static final String sk = "zzNMgEl8pDpDBEQLVpawuQLRzRnYkVh1";
+    private static final String imageFolder = "src/main/resources/static/BotImage";
+    private final Qianfan qianfan = new Qianfan(TYPE_OAUTH,ak, sk);
+
+    //输入生成图像的prompt，返回图像的名称
+    public String generateImage(String prompt) {
+        // 调用文生图模型
+        Text2ImageResponse response = qianfan.text2Image().model("Stable-Diffusion-XL")
+                .prompt(prompt)
+                .execute();
+        byte[] image_data = response.getData().get(0).getImage();
+        return saveImage(image_data, imageFolder);
+    }
+
+    public static String saveImage(byte[] imageData, String folderPath) {
+        String fileName = UUID.randomUUID() + ".png"; // 生成唯一文件名
+        File outputFile = new File(folderPath, fileName); // 在指定文件夹中创建文件
+        try {
+            BufferedImage img = ImageIO.read(new ByteArrayInputStream(imageData));
+            ImageIO.write(img, "png", outputFile); // 保存图像
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return outputFile.getName();
+    }
+}
