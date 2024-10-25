556186b805a788c10035188b0b0fde8eaa4ed854
Bronyadog <119098838+Bronyadog@users.noreply.github.com>
2024-10-25 11:12:55 +0800

    修改一些界面

---
+++ src/components/RobotDisplay.vue
@@ -0,0 +1,50 @@
+<!-- src/components/RobotCard.vue -->
+<template>
+  <div class="media">
+    <figure class="media-left">
+      <el-avatar :size="64" :src="robot.avatarURl" alt="Placeholder image" />
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong class="title is-6">{{ robot.name }}</strong>
+          <br>{{ robot.description }}
+          <span style="display: flex; align-items: center; font-size: 15px; font-weight: 500">
+            每条消息花费token: <strong>{{ robot.tokenCost }}</strong>
+          </span>
+        </p>
+      </div>
+    </div>
+    <div class="media-right" v-if="showButton">
+      <el-button text size="large" round @click="review(robot.botId)">撰写评价<el-icon><EditPen /></el-icon></el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { EditPen } from "@element-plus/icons-vue";
+
+export default {
+  name: 'RobotDisplay',
+  components: { EditPen },
+  props: {
+    robot: {
+      type: Object,
+      required: true
+    },
+    showButton: {
+      type: Boolean,
+      default: true
+    }
+  },
+  methods: {
+    review(botId) {
+      this.$emit('review', botId);
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Add any specific styles for RobotCard here */
+</style>
