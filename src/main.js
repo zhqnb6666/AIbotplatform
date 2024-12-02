1a7a1e14bd1ed9e2a272b4eb526cc3a000afe951
kzq <119098838+Bronyadog@users.noreply.github.com>
2024-12-02 16:38:52 +0800

    全局异常捕获

--- src/main.js
+++ src/main.js
@@ -4,4 +4,12 @@ import router from '@/router';
 import store from '@/store';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
-createApp(App).use(ElementPlus).use(router).use(store).mount('#app');
+
+const app = createApp(App);
+
+app.config.errorHandler = (err, vm, info) => {
+    console.error('Error: ', err);
+    console.error('Info: ', info);
+};
+
+app.use(ElementPlus).use(router).use(store).mount('#app');
