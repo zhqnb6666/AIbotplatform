fb2ae3ea845545776ba773950eb7cbf525388f83
LYF <10584747954@qq.com>
2024-12-03 17:21:23 +0800

    admin奖励用户token接口

--- src/main/java/com/aibotplatform/service/AdminService.java
+++ src/main/java/com/aibotplatform/service/AdminService.java
@@ -1,5 +1,8 @@
 package com.aibotplatform.service;
 
+import org.springframework.web.bind.annotation.RequestParam;
+
 public interface AdminService {
     String exportInfo();
+    void rewardUser(Long userId, Long token);
 }
